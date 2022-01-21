931a6c015fb2bc840f23bfe13a0ad833df07501e
Zakir Hossen <73053667+zakirhossen23@users.noreply.github.com>
2022-01-21 15:05:56 +0600

    fixed home text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 			<div className="row">
 				<div style={{ 'width': '690px' }} className="col">
 					<div className="text-center">
-						<h4>DemeterGift uses the power of the Metis Layer 2 infrastructure to create the most easy, transparent, fun and digital charity auction on the web!</h4>
+						<h4>DemeterGift uses the power of the Everscale infrastructure to create the most easy, transparent, fun and digital charity auction on the web!</h4>
 					</div>
 				</div>
 			</div>
